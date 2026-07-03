9b52e4ba3214ef32f333542ad3a8103647adb3b8
Spectacled Bear Productions <tfranky05@gmail.com>
2026-07-03 22:46:32 +0000

    game eng

--- script.js
+++ script.js
@@ -1,12 +1,17 @@
 function checkInput() {
-    let userinput = document.getElementById("userinput").value;
+  let userinput = document.getElementById("userinput").value;
 
-    if (userinput === "Displaysky") {
-        document.getElementById("box").style.display = "block";
-    }
+  if (userinput === "Displaysky") {
+    document.getElementById("box").style.display = "block";
+  }
 
-    if (userinput === "Displaygrass") {
-        document.getElementById("skybox").style.display = "block";
+  if (userinput === "Displaygrass") {
+    document.getElementById("skybox").style.display = "block";
+  }
+
+    if (userinput === "Displayblack") {
+      document.getElementById("black").style.display = "block";
     }
-}
+  }
+
 
