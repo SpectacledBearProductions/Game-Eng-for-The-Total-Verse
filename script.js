8b1856f61f5cd209244b598cf83ddacce829eb8a
Spectacled Bear Productions <tfranky05@gmail.com>
2026-07-11 00:29:02 +0000

    got rid of commands

--- script.js
+++ script.js
@@ -1,17 +1,2 @@
-function checkInput() {
-  let userinput = document.getElementById("userinput").value;
-
-  if (userinput === "Displaysky") {
-    document.getElementById("box").style.display = "block";
-  }
-
-  if (userinput === "Displaygrass") {
-    document.getElementById("skybox").style.display = "block";
-  }
-
-    if (userinput === "Displayblack") {
-      document.getElementById("black").style.display = "block";
-    }
-  }
-
+'use strict'
 
